af962281268f1ee274c26f4d7289de34772e9133
manya13 <zmeika06@list.ru>
2018-12-18 18:31:15 +0300

    Исправления после 1 попытки

--- js/gllacy.js
+++ js/gllacy.js
@@ -10,25 +10,50 @@ var form = popup.querySelector(".feedback-form");
 var username = popup.querySelector("[name=feedback-name]");
 var email = popup.querySelector("[name=feedback-mail]");
 var comment = popup.querySelector("[name=feedback-text]");
+var isStorageSupport = true;
+var storageName = "";
+var storageMail = "";
+
+try {
+  storageName = localStorage.getItem("login");
+  storageMail = localStorage.getItem("email");
+} catch (err) {
+  isStorageSupport = false;
+}
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal-show");
   overlay.classList.add("feedback-overlay-show");
-  name.focus();
+  if (storage) {
+      username.value = storageName;
+      email.value = storageMail;
+      comment.focus();
+    } else {
+      username.focus();
+    }
 });
 
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
   overlay.classList.remove("feedback-overlay-show");
 });
 
 form.addEventListener("submit", function (evt) {
-  if (!username.value || !email.value) {
+  if (!username.value || !email.value || !comment.value) {
     evt.preventDefault();
-    console.log("Для отправки сообщения неободимо заполнить все поля");
-  }
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+    console.log("Для отправки сообщения необходимо заполнить все поля");
+  } else {
+      if (isStorageSupport) {
+        localStorage.setItem("login", username.value);
+        localStorage.setItem("email", email.value);
+      }
+    }
 });
 
 window.addEventListener("keydown", function (evt) {
@@ -36,6 +61,7 @@ window.addEventListener("keydown", function (evt) {
     evt.preventDefault();
     if (popup.classList.contains("modal-show")) {
       popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
     }
   }
 });
